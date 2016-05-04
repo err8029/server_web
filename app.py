546c11762ce47acad0c2ada8bd6b6cc38c0878eb
Your Name <you@example.com>
2016-05-04 20:43:11 +0200

    comments done

--- app.py
+++ app.py
@@ -1,17 +1,61 @@
 import json
 from flask import Flask, jsonify, abort, make_response
 
-posts = json.dumps(posts)
+
+posts=[
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+  }
+]
+
+comments =[
+  {
+    "postId": 1,
+    "id": 1,
+    "name": "id labore ex et quam laborum",
+    "email": "Eliseo@gardner.biz",
+    "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+  },
+  {
+    "postId": 1,
+    "id": 2,
+    "name": "quo vero reiciendis velit similique earum",
+    "email": "Jayne_Kuhic@sydney.com",
+    "body": "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
+  },
+  {
+    "postId": 1,
+    "id": 3,
+    "name": "odio adipisci rerum aut animi",
+    "email": "Nikita@garfield.biz",
+    "body": "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione"
+  },
+  {
+    "postId": 1,
+    "id": 4,
+    "name": "alias odio sit",
+    "email": "Lew@alysha.tv",
+    "body": "non et atque\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\nquia voluptas consequuntur itaque dolor\net qui rerum deleniti ut occaecati"
+  },
+]
 
 
 app = Flask(__name__)
-
-@app.route('/posts/<int:post_id>', methods=['GET'])
-def get_task(post_id):
-    post = [post for post in posts if post['id'] == post_id]
-    if len(post) == 0:
-        abort(404)
-    return jsonify({'post': post[0]})
 
 @app.errorhandler(404)
 def not_found(error):
@@ -20,6 +64,15 @@ def not_found(error):
 @app.errorhandler(400)
 def bad_request(error):
     return make_response(jsonify({'error': 'Bad request'}), 400)
+
+#-----------------------------------------------------------------------------------------------------------------------------
+
+@app.route('/posts/<int:post_id>', methods=['GET'])
+def get_task(post_id):
+    post = [post for post in posts if post['id'] == post_id]
+    if len(post) == 0:
+        abort(404)
+    return jsonify({'post': post[0]})
 
 @app.route('/posts', methods=['POST'])
 def create_post():
@@ -34,6 +87,69 @@ def create_post():
     posts.append(post)
     return jsonify({'post': post}), 201
 
+@app.route('/posts/<int:post_id>', methods=['PUT'])
+def update_post(post_id):
+    post = [post for post in posts if post['id'] == post_id]
+    if len(post) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    post[0]['title'] = request.json.get('title', post[0]['title'])
+    post[0]['body'] = request.json.get('body', post[0]['body'])
+    return jsonify({'post': post[0]})
+
+@app.route('/posts/<int:post_id>', methods=['DELETE'])
+def delete_post(post_id):
+    post = [post for post in posts if post['id'] == post_id]
+    if len(post) == 0:
+        abort(404)
+    posts.remove(post[0])
+    return jsonify({'result': True})
+
+#-----------------------------------------------------------------------------------------------------------------------------
+
+@app.route('/comments/<int:comment_id>', methods=['GET'])
+def get_task_Comments(comment_id):
+    comment = [comment for comment in comments if comment['id'] == comment_id]
+    if len(comment) == 0:
+        abort(404)
+    return jsonify({'post': comment[0]})
+
+app.route('/comments', methods=['POST'])
+def create_post_Comments():
+    if not request.json or not 'title' in request.json or not 'userId' in request.json or not 'body' in request.json:
+        abort(400)
+    comment = {
+        'id': 1,
+        'id': posts[-1]['id'] + 1,
+        'userId': request.json['userId'],
+        'title': request.json['title'],
+        'body': request.json['body'],
+    }
+    comments.append(comment)
+    return jsonify({'post': comment}), 201
+
+app.route('/comments/<int:comment_id>', methods=['PUT'])
+def update_post_Comments(comment_id):
+    comment = [comment for comment in comments if comment['id'] == comment_id]
+    if len(post) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    comment[0]['title'] = request.json.get('title', comment[0]['title'])
+    comment[0]['body'] = request.json.get('body', comment[0]['body'])
+    return jsonify({'post': comment[0]})
+
+@app.route('/comments/<int:post_id>', methods=['DELETE'])
+def delete_post_Comments(comment_id):
+    comment = [comment for comment in comments if comment['id'] == comment_id]
+    if len(comment) == 0:
+        abort(404)
+    comments.remove(comment[0])
+    return jsonify({'result': True})
+
+
+
+
 if __name__ == "__main__":
      app.run('0.0.0.0')
-     app.run(debug = True)
