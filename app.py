ec593097ce9e074ce68aa8e52d0a5b6d28c526c9
Your Name <you@example.com>
2016-05-04 19:48:04 +0200

    test with post and get

---
+++ app.py
@@ -0,0 +1,36 @@
+import flask
+
+from flask import Flask, jsonify, abort, make_response
+
+app = Flask(__name__)
+
+@app.route('/posts/<int:post_id>', methods=['GET'])
+def get_task(post_id):
+    post = [post for post in posts if post['id'] == post_id]
+    if len(post) == 0:
+        abort(404)
+    return jsonify({'post': post[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}), 400)
+
+@app.route('/posts', methods=['POST'])
+def create_post():
+    if not request.json or not 'title' in request.json or not 'userId' in request.json or not 'body' in request.json:
+        abort(400)
+    post = {
+        'id': posts[-1]['id'] + 1,
+        'userId': request.json['userId'],
+        'title': request.json['title'],
+        'body': request.json['body'],
+    }
+    posts.append(post)
+    return jsonify({'post': post}), 201
+
+if __name__ == "__main__":
+     app.run('0.0.0.0')
